ecd36c96535353ad37c50ea49ab2b8b4cf3e69e5
Gajanan Mallik <gajanan.mallik@tdcx.com>
2022-08-29 21:46:32 +0530

    Peak Index in a Mountain Array - Binary Search

---
+++ array/peakIndexInMountainArray.js
@@ -0,0 +1,26 @@
+//Pick Index in a Mountain Array
+// LeetCode - 852 - Binary Search
+
+function peakIndexInMountainArray(arr, size) {
+    let s = 0, e = size - 1;
+    let mid = parseInt(s + (e - s) / 2);
+    while(s < e) {
+        if(arr[mid] < arr[mid + 1]) {
+            s = mid + 1;
+        } else {
+            e = mid;
+        }
+
+        mid = parseInt(s + (e - s) / 2);
+    }
+
+    return s;
+}
+
+function main() {
+    const arr = [0, 10, 5, 2];
+    const ans = peakIndexInMountainArray(arr, arr.length);
+    console.log("Pick Index of array is", ans);
+}
+
+main();
